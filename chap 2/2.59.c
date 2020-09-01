c64ed7205c8b979fd2854f3172006165dc99e5d9
xsw0 <xsw.mail.0@gmail.com>
2020-09-01 17:21:06 +0800

    Update 2.59.c

--- chap 2/2.59.c
+++ chap 2/2.59.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdint.h>
 
-uint32_t leastByteX(uint32_t x, uint32_t y)
+uint32_t YbutLSB(uint32_t x, uint32_t y)
 {
     return (0xff & x) | (~0xff & y);
 }
 
 int main(int argc, char *argv[])
 {
-    printf("%.8x\n", leastByteX(0x89ABCDEF, 0x76543210));
+    printf("%.8x\n", YbutLSB(0x89ABCDEF, 0x76543210));
     return 0;
 }
